04e09e2c0ad9354c606c7b876771045de1f5c653
Daniel <whitestripe7773@gmail.com>
2020-05-19 23:25:27 +0200

    Bug fix, vid/pic html is now both dynamically created with JS

--- script.js
+++ script.js
@@ -4,24 +4,27 @@
 function getPictureOfTheDay(){
     $.getJSON("https://api.nasa.gov/planetary/apod?api_key=cahfmjH52VpP3NcB736fPQzIw0PYS8sGSd4N0Idv", showdata = function(data){
 
-        var generate = document.getElementById("generate-here");
-        var htmlGenerated = '<div class="iframe-container"><iframe class="video" frameborder="0" allowfullscreen></iframe></div>';
+         // Getting the title of the picture
+         var title = data.title;
+         // Getting the picture/Video
+         var picvid = data.url;
+         //Getting the explanation from the JSON object as String
+         var explanation = data.explanation;
+         // Getting the date from the JSON object as String
+         var date = data.date; 
 
-        // Getting the title of the picture
-        var title = data.title;
-        // Getting the picture/Video
-        var picvid = data.url;
-        //Getting the explanation from the JSON object as String
-        var explanation = data.explanation;
-        // Getting the date from the JSON object as String
-        var date = data.date;
+        var generate = document.getElementById("generate-vidpic-here");
+
+        var generatedVideo = '<div class="iframe-container"><iframe class="video" frameborder="0" allowfullscreen></iframe></div>';
+        var generatedPicture = '<div class="iframe-container"><img class="picture"></div><br><a href=' + picvid + '>See full picture here</a>';
 
         $(".title").text(title);     
         if (picvid.includes("youtube")){
-            generate.innerHTML = htmlGenerated;
+            generate.innerHTML += generatedVideo;
             $(".video").attr("src", picvid); 
         }
         else{
+            generate.innerHTML += generatedPicture;
             $(".picture").attr("src", picvid); 
         }
         $(".explanation").text(explanation);
