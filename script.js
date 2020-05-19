5367e195abb7f22b379adef35082874b2b2ab8b0
Daniel <whitestripe7773@gmail.com>
2020-05-19 22:51:13 +0200

    Create iFrame html with JS

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 function getPictureOfTheDay(){
     $.getJSON("https://api.nasa.gov/planetary/apod?api_key=cahfmjH52VpP3NcB736fPQzIw0PYS8sGSd4N0Idv", showdata = function(data){
 
+        var generate = document.getElementById("generate-here");
+        var htmlGenerated = '<div class="iframe-container"><iframe class="video" frameborder="0" allowfullscreen></iframe></div>';
+
         // Getting the title of the picture
         var title = data.title;
         // Getting the picture/Video
@@ -15,6 +18,7 @@ function getPictureOfTheDay(){
 
         $(".title").text(title);     
         if (picvid.includes("youtube")){
+            generate.innerHTML = htmlGenerated;
             $(".video").attr("src", picvid); 
         }
         else{
@@ -23,4 +27,4 @@ function getPictureOfTheDay(){
         $(".explanation").text(explanation);
         $(".date").text(date);
     });
-    }
+}
